19534b494a695bc17ae6790302068dbd8b4102e2
Jort Broekhof <jortdus@gmail.com>
2018-05-22 11:19:44 +0200

    update v.2

---
+++ script/next.js
@@ -0,0 +1,20 @@
+// var currentTab = '';
+// var menu = document.querySelector('.menu');
+// function showTab(event, id) {
+//     console.log(id);
+//     if (currentTab != '') {
+//         $('#tab-' + currentTab).hide();
+//     }
+//     $('#tab-' + id).show();
+//     currentTab = id;
+// }
+
+// var currentStep = '';
+
+// function nextStep(event, id) {
+//     if (currentStep != '') {
+
+//     }
+// }
+
+// showTab(event, 'home');
